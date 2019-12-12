8e70a39dcfe5da07f0ba826abde2d7dc9ca9c213
Daniel M <dansegund@gmail.com>
2019-12-12 00:13:32 -0500

    Fixed for loop to include 'var' keyword.

--- chapter02/grid.js
+++ chapter02/grid.js
@@ -1,8 +1,8 @@
 var grid = "";
 var size = 8;
 
-for (line = 0; line < size; line++) {
-	for (chars = 0; chars < size; chars++) {
+for (var line = 0; line < size; line++) {
+	for (var chars = 0; chars < size; chars++) {
 		if ((line + chars) % 2 == 0) {
 			grid += " ";
 		} else {
